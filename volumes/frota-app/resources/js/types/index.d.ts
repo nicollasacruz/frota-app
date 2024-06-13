269397a4ff086a3665d1e50ae15106c418dde3f7
Nicollas Cruz <nicollasacruz@gmail.com>
2024-06-14 00:27:43 +0100

    ajuste tipos do User

--- volumes/frota-app/resources/js/types/index.d.ts
+++ volumes/frota-app/resources/js/types/index.d.ts
@@ -5,6 +5,12 @@ export interface User {
     name: string;
     email: string;
     email_verified_at: string;
+    created_at: string;
+    updated_at: string;
+    roles: string[];
+    IBAN: string;
+    NIF: string;
+    phone: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
